a22480b8f9e507aaa3e8d4cfbb43e5dee0ffe99f
Fernando Basello <fernando.basello@poincenot.com>
2025-07-17 17:57:40 -0300

    custom images para tener script de register y hb en redis

---
+++ android-emulators/dockerfiles/scripts/register_and_hb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REDIS_HOST=${REDIS_HOST:-redis}
+REDIS_PORT=${REDIS_PORT:-6379}
+EMULATOR_PORT=${EMULATOR_PORT:-5555}
+TTL=${TTL:-15}
+INTERVAL=${INTERVAL:-10}
+
+POD_IP=$(hostname -i | awk '{print $1}')
+POD_NAME=$(hostname)
+
+KEY="emulator:${POD_NAME}"
+
+echo "🔧 Registrando $KEY en Redis @$REDIS_HOST:$REDIS_PORT"
+
+while true; do
+  redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" HSET "$KEY" host "$POD_IP" port "$EMULATOR_PORT" state "idle"
+  redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" EXPIRE "$KEY" "$TTL"
+  echo "💓 Heartbeat enviado para $KEY"
+  sleep "$INTERVAL"
+done
