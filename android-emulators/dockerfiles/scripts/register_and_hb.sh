cfa674266ed5baf6ee7042eead6d4e8e9fbe4bc1
Fernando Basello <fernando.basello@poincenot.com>
2025-07-19 00:16:52 -0300

    nueva arch para los emus asi puedo tener replicas

--- android-emulators/dockerfiles/scripts/register_and_hb.sh
+++ android-emulators/dockerfiles/scripts/register_and_hb.sh
@@ -2,21 +2,17 @@
 
 REDIS_HOST=${RHOST:-redis}
 REDIS_PORT=${RPORT:-6379}
-EMULATOR_PORT=${EMULATOR_PORT:-5555}
 TTL=${TTL:-15}
 INTERVAL=${INTERVAL:-10}
 
-POD_IP=$(hostname -i | awk '{print $1}')
 POD_NAME=$(hostname)
 
-KEY="emulator:${POD_NAME}"
-
-echo "🔧 Registrando $KEY en Redis @$REDIS_HOST:$REDIS_PORT"
+echo "🔧 Registrando $POD_NAME en Redis @$REDIS_HOST:$REDIS_PORT"
 
 while true; do
-  echo  redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" HSET "$KEY" host "$POD_IP" port "$EMULATOR_PORT" state "idle" 
-  redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" HSET "$KEY" host "$POD_IP" port "$EMULATOR_PORT" state "idle"
-  redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" EXPIRE "$KEY" "$TTL"
-  echo "💓 Heartbeat enviado para $KEY"
+  echo redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" HSET "$POD_NAME" state "idle"
+  redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" HSET "$POD_NAME" state "idle"
+  redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" EXPIRE "$POD_NAME" "$TTL"
+  echo "💓 Heartbeat enviado para $POD_NAME"
   sleep "$INTERVAL"
 done
