222b7ceaf67daadaa4b6c717d1d3cd584e3fec1e
Fernando Basello <fernando.basello@poincenot.com>
2025-07-20 14:49:53 -0300

    trato de forzar kvm

--- android-emulators/dockerfiles/scripts/wrapper.sh
+++ android-emulators/dockerfiles/scripts/wrapper.sh
@@ -3,5 +3,5 @@
 # Inicia el heartbeat en segundo plano
 /usr/local/bin/register_and_heartbeat.sh &
 
-# Ejecuta el entrypoint original del emulador
-exec /home/androidusr/docker-android/mixins/scripts/run.sh "$@"
+# Ejecuta el entrypoint original con forzado de KVM
+exec /home/androidusr/docker-android/mixins/scripts/run.sh "$@" -accel kvm
