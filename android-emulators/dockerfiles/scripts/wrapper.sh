1217f7a97090e14883f36ef16de9546d83cf0faa
Fernando Basello <fernando.basello@poincenot.com>
2025-07-20 18:39:51 -0300

    saco el wrapper

--- android-emulators/dockerfiles/scripts/wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Inicia el heartbeat en segundo plano
-/usr/local/bin/register_and_heartbeat.sh &
-
-# Ejecuta el entrypoint original con forzado de KVM
-exec /home/androidusr/docker-android/mixins/scripts/run.sh "$@" -accel kvm
